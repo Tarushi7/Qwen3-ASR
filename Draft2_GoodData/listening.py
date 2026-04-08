4c73966e02c27be809f9a1665a9221b0e40c4bd6
Tarushi Gangwar <tarushigangwar@gmail.com>
2026-04-08 07:01:46 -0400

    extracted wav files for evaluation and removed old

--- Draft2_GoodData/listening.py
+++ Draft2_GoodData/listening.py
@@ -10,10 +10,10 @@ from datasets import load_from_disk
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ta_30_dev" #tamil
 
 #60s files
-path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_en_60_dev"  #english
-#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_zh_60_dev" #chinese
+#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_en_60_dev"  #english
+path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_zh_60_dev" #chinese
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ms_60_dev" #malay
-#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ta_30_dev" #tamil
+#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ta_60_dev" #tamil
 
 output_folder = 'extracted_wavs'
 
@@ -27,11 +27,11 @@ print(f"Starting conversion of {len(ds)} files...")
     
 
 #for individual files:
-target_index = 3
+target_index = 611
 entry = ds[target_index]
 audio = entry['context']['audio']
 
-file_path = os.path.join(output_folder, f"check_en_60_index_{target_index}.wav")
+file_path = os.path.join(output_folder, f"check_zh_60_index_{target_index}.wav")
 sf.write(file_path, audio['array'], audio['sampling_rate'])
 
 print(f"Extracted index {target_index} to: {file_path}")
