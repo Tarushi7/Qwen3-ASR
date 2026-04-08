71b21fa656f897215e087e4f05eb8adec2ef1149
Tarushi Gangwar <tarushigangwar@gmail.com>
2026-04-08 07:00:42 -0400

    extracted wav files for evaluation and removed old

--- Draft2_GoodData/listening.py
+++ Draft2_GoodData/listening.py
@@ -7,11 +7,13 @@ from datasets import load_from_disk
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_en_30_dev"  #english
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_zh_30_dev" #chinese
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ms_30_dev" #malay
+#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ta_30_dev" #tamil
 
 #60s files
 path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_en_60_dev"  #english
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_zh_60_dev" #chinese
 #path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ms_60_dev" #malay
+#path = "/home/q-wang/data/hf_egs/Emotional-YTB-MY_ta_30_dev" #tamil
 
 output_folder = 'extracted_wavs'
 
@@ -22,22 +24,10 @@ ds = load_from_disk(path)
 
 print(f"Starting conversion of {len(ds)} files...")
 
-# #for all files:
-# for i, entry in enumerate(ds):
-#     audio_data = entry['context']['audio'] 
-    
-#     waveform = audio_data['array']
-#     sample_rate = audio_data['sampling_rate']
-    
-#     file_path = os.path.join(output_folder, f"audio_{i}.wav")
-    
-#     sf.write(file_path, waveform, sample_rate)
-#     if i % 100 == 0:
-#         print(f"Converted {i} files...")
     
 
 #for individual files:
-target_index = 288
+target_index = 3
 entry = ds[target_index]
 audio = entry['context']['audio']
 
